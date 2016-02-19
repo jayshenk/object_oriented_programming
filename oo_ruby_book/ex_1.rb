41d5cdfd516cf0980b0307edeefd54c7e69eb80f
Jay Shenk <jay.shenk@gmail.com>
2016-02-18 21:22:11 -0500

    Initialize Repository

---
+++ oo_ruby_book/ex_1.rb
@@ -0,0 +1,8 @@
+# We create an object by first defining a class and then instantiating it with
+# the .new method to create an instance, also known as an object.
+
+# For example:
+class ZooAnimal
+end
+
+tiger = ZooAnimal.new
