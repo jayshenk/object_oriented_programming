68dde31217b6475bd09a38e33512e2c5f8fdfecf
Jay Shenk <jay.shenk@gmail.com>
2016-02-22 18:06:42 -0500

    Complete Inheritance Exercises

---
+++ oo_ruby_book/inheritance/ex_8.md
@@ -0,0 +1,3 @@
+The problem is that you are trying to call a private method directly.
+You can fix this by creating a public method that calls the private method
+or by changing the `hi` method to a public method.
---
+++ oo_ruby_book/inheritance/student.rb
@@ -0,0 +1,22 @@
+class Student
+  attr_reader :name
+
+  def initialize(n, g)
+    @name  = n
+    @grade = g
+  end
+
+  def better_grade_than?(other_student)
+    grade > other_student.grade
+  end
+
+  protected
+
+  def grade
+    @grade
+  end
+end
+
+joe = Student.new("Joe", 90)
+bob = Student.new("Bob", 84)
+puts "Well done!" if joe.better_grade_than?(bob)
