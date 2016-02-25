152a1cab723ff0bd045fe0367d5028297c1906d6
Jay Shenk <jay.shenk@gmail.com>
2016-02-25 06:42:08 -0500

    Complete Lizard Spock Bonus Feature

---
+++ lesson_2/oo_rps/lizard_spock.rb
@@ -0,0 +1,125 @@
+class Move
+  VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+  HIERARCHY = { 'rock' => %w(scissors lizard),
+                'paper' => %w(rock spock),
+                'scissors' => %w(paper lizard),
+                'lizard' => %w(paper spock),
+                'spock' => %w(scissors rock) }
+
+  def initialize(value)
+    @value = value
+  end
+
+  def >(other_move)
+    HIERARCHY[@value].include?(other_move.to_s)
+  end
+
+  def <(other_move)
+    HIERARCHY[other_move.to_s].include?(@value)
+  end
+
+  def to_s
+    @value
+  end
+end
+
+class Player
+  attr_accessor :move, :name
+
+  def initialize
+    set_name
+  end
+end
+
+class Human < Player
+  def set_name
+    n = ""
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = n
+  end
+
+  def choose
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, scissors, lizard, or spock:"
+      choice = gets.chomp
+      break if Move::VALUES.include? choice
+      puts "Sorry, invalid choice."
+    end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+  end
+
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
+  end
+end
+
+# Game Orchestration Engine
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors, Lizard, Spock!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Good bye!"
+  end
+
+  def display_moves
+    puts "#{human.name} chose #{human.move}"
+    puts "#{computer.name} chose #{computer.move}"
+  end
+
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include? answer.downcase
+      puts "Sorry, must be y or n."
+    end
+    return false if answer.downcase == 'n'
+    return true if answer.downcase == 'y'
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      human.choose
+      computer.choose
+      display_moves
+      display_winner
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
