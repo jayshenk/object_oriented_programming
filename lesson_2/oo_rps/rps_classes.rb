e33a6137596b0a9317e694d23b618922afec30a1
Jay Shenk <jay.shenk@gmail.com>
2016-02-28 14:44:11 -0500

    Complete RPS Classes Bonus Feature

---
+++ lesson_2/oo_rps/rps_classes.rb
@@ -0,0 +1,176 @@
+class Move
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def scissors?
+    self.class == Scissors
+  end
+
+  def rock?
+    self.class == Rock
+  end
+
+  def paper?
+    self.class == Paper
+  end
+
+  def to_s
+    @value
+  end
+end
+
+class Rock < Move
+  def initialize
+    @value = 'rock'
+  end
+
+  def >(other_move)
+    other_move.scissors?
+  end
+
+  def <(other_move)
+    other_move.paper?
+  end
+end
+
+class Paper < Move
+  def initialize
+    @value = 'paper'
+  end
+
+  def >(other_move)
+    other_move.rock?
+  end
+
+  def <(other_move)
+    other_move.scissors?
+  end
+end
+
+class Scissors < Move
+  def initialize
+    @value = 'scissors'
+  end
+
+  def >(other_move)
+    other_move.paper?
+  end
+
+  def <(other_move)
+    other_move.rock?
+  end
+end
+
+class Player
+  attr_accessor :move, :name
+
+  def initialize
+    set_name
+  end
+end
+
+class Human < Player
+  def set_name
+    n = ""
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = n
+  end
+
+  def choose
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if Move::VALUES.include? choice
+      puts "Sorry, invalid choice."
+    end
+    self.move = case choice
+                when 'rock'
+                  Rock.new
+                when 'paper'
+                  Paper.new
+                when 'scissors'
+                  Scissors.new
+                end
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+  end
+
+  def choose
+    self.move = case Move::VALUES.sample
+                when 'rock'
+                  Rock.new
+                when 'paper'
+                  Paper.new
+                when 'scissors'
+                  Scissors.new
+                end
+  end
+end
+
+# Game Orchestration Engine
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
+  end
+
+  def display_moves
+    puts "#{human.name} chose #{human.move}"
+    puts "#{computer.name} chose #{computer.move}"
+  end
+
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include? answer.downcase
+      puts "Sorry, must be y or n."
+    end
+    return false if answer.downcase == 'n'
+    return true if answer.downcase == 'y'
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      human.choose
+      computer.choose
+      display_moves
+      display_winner
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
