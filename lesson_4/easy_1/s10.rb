cdec4574b918f5c2c3403d2258870cec121ce999
Jay Shenk <jay.shenk@gmail.com>
2016-03-02 18:44:39 -0500

    Complete Easy 1 Exercises

---
+++ lesson_4/easy_1/s10.rb
@@ -0,0 +1,3 @@
+# You would need to call Bag.new with two arguments. 
+
+Bag.new('red', 'leather')
---
+++ lesson_4/easy_1/s6.rb
@@ -0,0 +1,9 @@
+# You can add attr_accessor :volume
+
+class Cube
+  attr_accessor :volume
+
+  def initialize(volume)
+    @volume = volume
+  end
+end
